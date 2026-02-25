7d479a2c9bac6375eda372f37a06c3970c56e393
Ricciolo2001 <31922670+Ricciolo2001@users.noreply.github.com>
2026-02-25 17:57:49 +0100

    Add BLE beacon functionality with LED indicator
    This code initializes a BLE beacon and toggles an LED on pin 8.

---
+++ BLE_beacon.ino
@@ -0,0 +1,30 @@
+// This code is mounted on a BLE beacon used for testing
+
+#include <NimBLEDevice.h>  // Lightweight BLE library for esp32-C3
+
+void setup() {
+  Serial.begin(115200);
+  delay(1000);
+
+  Serial.println("Starting BLE Beacon...");
+
+  pinMode(8, OUTPUT);
+  digitalWrite(8, HIGH);
+  delay(100);
+
+  NimBLEDevice::init("BLE_Beacon");
+  NimBLEDevice::setPower(ESP_PWR_LVL_P9); 
+
+  NimBLEAdvertising* pAdvertising = NimBLEDevice::getAdvertising();
+  pAdvertising->start();
+
+  delay(1000);
+  Serial.println("Beacon active.");
+}
+
+void loop() {
+  digitalWrite(8, LOW);
+  delay(100);
+  digitalWrite(8, HIGH);
+  delay(900);
+}
