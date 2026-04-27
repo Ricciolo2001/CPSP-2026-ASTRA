756f5755e0a1c849a48c5c64613b36cb63376ed1
VaiTon <eyadlorenzo@gmail.com>
2026-04-27 13:52:34 +0200

    beacon-esp32: Convert to PlatformIO

---
+++ beacon-esp32-c3/src/BLE_beacon.cpp
@@ -0,0 +1,29 @@
+#include <Arduino.h>
+#include <NimBLEAddress.h>
+#include <NimBLEDevice.h>
+
+void setup()
+{
+  Serial.begin(115200);
+  delay(1000);
+
+  Serial.println("Starting BLE Beacon...");
+
+  NimBLEDevice::init("ASTRA BEACON");
+  NimBLEDevice::setPower(ESP_PWR_LVL_P21);
+
+  NimBLEAdvertising *pAdvertising = NimBLEDevice::getAdvertising();
+  pAdvertising->start();
+
+  NimBLEAddress addr = NimBLEDevice::getAddress();
+  Serial.print("Beacon MAC Address: ");
+  Serial.println(addr.toString().c_str());
+
+  delay(1000);
+  Serial.println("Beacon active.");
+}
+
+void loop()
+{
+  delay(1000);
+}
